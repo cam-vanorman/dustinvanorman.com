350e117a23d145f0b635f097416e912c0ebec31e
Cameron Van Orman <cameron@efellemedia.com>
2016-02-29 13:18:10 -0800

    ^ removing unused sections

--- template-main.php
+++ template-main.php
@@ -7,9 +7,5 @@
 	<?php get_template_part('templates/callout'); ?>
 
 	<?php get_template_part('templates/about'); ?>
-
-	<?php get_template_part('templates/services'); ?>
-
-	<?php get_template_part('templates/portfolio'); ?>
 	
 	<?php get_template_part('templates/call-to-action'); ?>
