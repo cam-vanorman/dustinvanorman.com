e61b0ddc1c8d8966f4a97aa03cbc67c21bf9b854
Cameron Van Orman <cameron@efellemedia.com>
2016-02-29 13:34:08 -0800

    ^ footer text

--- templates/footer.php
+++ templates/footer.php
@@ -1,17 +1,17 @@
 	<footer id="contact" class="content-info" role="contentinfo">
 		<div class="container-fluid">
 			<section class="copyright-2015">
-				<div class="panel panel-default text-center">
-					<div class="panel-body bg-coal">
-						<!-- <ul class="list-inline">
-							<li><a class="btn-social btn-outline" href="https://www.linkedin.com/in/vanormandesign" target="_blank"><i class="fab fab-linkedin-alt bt-fw"></i></a></li>
-							<li><a class="btn-social btn-outline" href="https://github.com/cam-vanorman" target="_blank"><i class="fab fab-github bt-fw"></i></a></li>
-							<li><a class="btn-social btn-outline" href="mailto:cameron.vanorman@gmail.com"><i class="btl bt-paper-plane bt-fw"></i></a></li>
-							<li><a class="btn-social btn-outline" href="https://open.spotify.com/user/1238806903" target="_blank"><i class="fab fab-spotify bt-fw"></i></a></li>
-						</ul> -->
+				<div class="row text-center">
+					<div class="col-xs-12">
+						<ul class="list-inline">
+							<li><a class="btn-social btn-outline" href="//www.linkedin.com/in/dustinvanorman" target="_blank"><i class="fab fab-linkedin-alt bt-fw"></i></a></li>
+							<!-- <li><a class="btn-social btn-outline" href="https://github.com/cam-vanorman" target="_blank"><i class="fab fab-github bt-fw"></i></a></li> -->
+							<!-- <li><a class="btn-social btn-outline" href="mailto:cameron.vanorman@gmail.com"><i class="btl bt-paper-plane bt-fw"></i></a></li> -->
+							<!-- <li><a class="btn-social btn-outline" href="https://open.spotify.com/user/1238806903" target="_blank"><i class="fab fab-spotify bt-fw"></i></a></li> -->
+						</ul>
 						<!-- <hr class="small"> -->
 					
-						<p class="text-center">Made with brotherly love <i title="love" class="love btl bt-heart bt-fw"></i> &amp; <i title="coffee" class="btl bt-mug bt-fw"></i> by <a href="//vanorman.design">Cameron Van Orman</a> in Seattle, WA.</p>
+						<p class="text-center">Made with brotherly <i title="love" class="love btl bt-heart bt-fw"></i> &amp; <i title="coffee" class="btl bt-mug bt-fw"></i> by <a class="text-light-green" href="//vanorman.design" target="_blank">Cameron Van Orman</a> in Seattle, WA.</p>
 						<p class="text-center">Copyright <?= date('Y'); ?> &copy; Dustin Van Orman</p>
 						<?php dynamic_sidebar('sidebar-footer'); ?>
 					</div>
