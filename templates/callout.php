34182645335029c747dd6210417edc60d3d276ab
Cameron Van Orman <cameron@efellemedia.com>
2016-03-01 00:04:21 -0800

    updating template

--- templates/callout.php
+++ templates/callout.php
@@ -13,9 +13,9 @@
                 <hr class="small">
                 <h4>Beijing, CN | 北京，中国</h4>
                 <ul class="list-inline">
-                    <li><a href="//www.linkedin.com/in/dustinvanorman" target="_blank"><i class="text-coal fab fab-linkedin-alt bt-fw"></i></a></li>
-                    <li><a href="//www.researchgate.net/profile/Dustin_Van_Orman" target="_blank"><img src="<?= esc_url(get_template_directory_uri('/')); ?>/dist/images/RG_Logo.png" alt="Research Gate Logo" title="Research Gate" width="35"></a></li>
-                    <!-- <li><a href="mailto:cameron.vanorman@gmail.com" class="btn-social btn-outline"><i class="text-light-green btl bt-paper-plane bt-fw"></i></a></li> -->
+                    <li><a href="//www.linkedin.com/in/dustinvanorman" target="_blank"><i class="text-coal fab fab-linkedin-alt bt-lg"></i></a></li>
+                    <li><a href="//www.researchgate.net/profile/Dustin_Van_Orman" target="_blank"><img src="<?= esc_url(get_template_directory_uri('/')); ?>/dist/images/RG_Logo.png" alt="Research Gate Logo" title="Research Gate" width="25"></a></li>
+                    <li><a href="//ningbo.academia.edu/DustinVanOrman" target="_blank"><img src="<?= esc_url(get_template_directory_uri('/')); ?>/dist/images/academia-logo-redesign-2015.svg" width="25"></i></a></li>
                     <!-- <li><a href="https://open.spotify.com/user/1238806903" class="btn-social btn-outline" target="_blank"><i class="text-light-green fab fab-spotify bt-fw"></i></a></li> -->
                 </ul>
             </div>
