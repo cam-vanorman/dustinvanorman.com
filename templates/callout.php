16b41fe363eadf7dc373572d5e7ec5419f54f089
Cameron Van Orman <cameron@efellemedia.com>
2016-02-29 23:26:53 -0800

    ^ template

--- templates/callout.php
+++ templates/callout.php
@@ -14,7 +14,7 @@
                 <h4>Beijing, CN | 北京，中国</h4>
                 <ul class="list-inline">
                     <li><a href="//www.linkedin.com/in/dustinvanorman" target="_blank" class="btn-social"><i class="text-mild-blue fab fab-linkedin-alt bt-fw"></i></a></li>
-                    <li><a href="//www.researchgate.net/profile/Dustin_Van_Orman" target="_blank" class="btn-social btn-outline"><img src="/dist/imags/RG_Logo.png" alt="Research Gate Logo" title="Research Gate" width="50"></a></li>
+                    <li><a href="//www.researchgate.net/profile/Dustin_Van_Orman" target="_blank" class="btn-social btn-outline"><img src="<?= esc_url(get_template_directory_uri('/')); ?>/dist/images/RG_Logo.png" alt="Research Gate Logo" title="Research Gate" width="50"></a></li>
                     <!-- <li><a href="mailto:cameron.vanorman@gmail.com" class="btn-social btn-outline"><i class="text-light-green btl bt-paper-plane bt-fw"></i></a></li> -->
                     <!-- <li><a href="https://open.spotify.com/user/1238806903" class="btn-social btn-outline" target="_blank"><i class="text-light-green fab fab-spotify bt-fw"></i></a></li> -->
                 </ul>
