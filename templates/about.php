a9c6d4d061897f3d7efd68b9291d3e4743c6237c
Cameron Van Orman <cameron@efellemedia.com>
2016-02-29 23:29:33 -0800

    ^ about

--- templates/about.php
+++ templates/about.php
@@ -14,10 +14,10 @@
 				<div class="col-lg-5 col-sm-12">
 					<img class="mar-b-20 mar-t-20 img-center img-responsive img-thumbnail " src="<?= esc_url(get_template_directory_uri('/')); ?>/dist/images/dvanorman-homepage.jpg" alt="Dustin Van Orman image" title="Dustin Van Orman" />
 				</div>
-				<div class="col-lg-7 col-sm-12 text-vertical-center">
-					<p class="lead">Dustin is devoted to a lifetime of deep learning, experiential living and love. Even as a kid, Dustin wanted to learn how the psycho-social self and the social world worked from all perspectives they could be learned from (impossible as it is). Therefore, for Dustin, learning language, culture, society, about the brain and the interplay between each of them became a lifelong mission that still drives him. In undergraduate, he studied Linguistics, Sociology and Spanish; then embarked on a self-education of sorts in both Spain and China hoping to come to understand these places from both insider’s and outsider’s perspectives. Most formative, perhaps, has been from May 2011 to the present, he has lived and worked in six Chinese cities, teaching English, Spanish, International Contemporary Issues, and research writing while independently learning Mandarin Chinese to working fluency as well as basic abilities in the dialects and languages from Baotou, Huzhou, Ningbo, Longquan and Beijing. From rural Inner Mongolia to suburban Beijing to urban Zhejiang, he has researched to understand what it really means to be Chinese, American, and a human within human civilization. While he will be the first to admit how epistemology deprived we all are (including himself), he views each day as an opportunity to fill in gaps in knowledge about our physical world. Today Dustin has a MA in Applied Linguistics, accumulated an 8 year teaching career and is hoping to continue onto his PhD soon. Contributing to the human pursuit of truth, self-improvement, health and happiness of those he loves are some general ways to describe the human that is called Dustin Sonny James Van Orman.</p>
-					<!-- <br /> -->
-					<!-- <p class="lead"><a class="" href="#aboutModal" data-toggle="modal">Read More <i class="btl bt-circle-arrow-right bt-fw"></i></a></p> -->
+				<div class="col-lg-7 col-sm-12">
+					<p class="lead">Dustin is devoted to a lifetime of deep learning, experiential living and love. Even as a kid, Dustin wanted to learn how the psycho-social self and the social world worked from all perspectives they could be learned from (impossible as it is). Therefore, for Dustin, learning language, culture, society, about the brain and the interplay between each of them became a lifelong mission that still drives him.</p>
+					<br />
+					<p class="lead"><a class="" href="#aboutModal" data-toggle="modal">Read More <i class="btl bt-circle-arrow-right bt-fw"></i></a></p>
 				</div>
 			</div>
 		</div>
@@ -36,7 +36,7 @@
 					</div>
 					<div class="row">
 						<div class="col-md-6">
-							<p class="lead">Even as a kid, Dustin wanted to learn how the psycho-social self and the social world worked from all perspectives they could be learned from (impossible as it is). Therefore, for Dustin, learning language, culture, society, about the brain and the interplay between each of them became a lifelong mission that still drives him. In undergraduate, he studied Linguistics, Sociology and Spanish; then embarked on a self-education of sorts in both Spain and China hoping to come to understand these places from both insider’s and outsider’s perspectives. Most formative, perhaps, has been from May 2011 to the present, he has lived and worked in six Chinese cities, teaching English, Spanish, International Contemporary Issues, and research writing while independently learning Mandarin Chinese to working fluency as well as basic abilities in the dialects and languages from Baotou, Huzhou, Ningbo, Longquan and Beijing.</p>
+							<p class="lead">In undergraduate, he studied Linguistics, Sociology and Spanish; then embarked on a self-education of sorts in both Spain and China hoping to come to understand these places from both insider’s and outsider’s perspectives. Most formative, perhaps, has been from May 2011 to the present, he has lived and worked in six Chinese cities, teaching English, Spanish, International Contemporary Issues, and research writing while independently learning Mandarin Chinese to working fluency as well as basic abilities in the dialects and languages from Baotou, Huzhou, Ningbo, Longquan and Beijing.</p>
 						</div>
 						<div class="col-md-6">    
 							<p class="lead">
