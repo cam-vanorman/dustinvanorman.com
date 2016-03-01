e5f69dac491be8b61a4a41b8008ee685a069cf6e
Cameron Van Orman <cameron@efellemedia.com>
2016-02-29 20:04:40 -0800

    ^ callout/header

--- templates/callout.php
+++ templates/callout.php
@@ -13,7 +13,7 @@
                 <hr class="small">
                 <h4>Beijing, CN | 北京，中國</h4>
                 <ul class="list-inline">
-                    <li><a href="//www.linkedin.com/in/dustinvanorman" target="_blank" class="btn-social btn-outline"><i class="text-mild-blue fab fab-linkedin-alt bt-fw"></i></a></li>
+                    <li><a href="//www.linkedin.com/in/dustinvanorman" target="_blank" class="btn-social"><i class="text-mild-blue fab fab-linkedin-alt bt-fw"></i></a></li>
                     <!-- <li><a href="https://github.com/cam-vanorman" target="_blank" class="btn-social btn-outline"><i class="text-light-green fab fab-github bt-fw"></i></a></li> -->
                     <!-- <li><a href="mailto:cameron.vanorman@gmail.com" class="btn-social btn-outline"><i class="text-light-green btl bt-paper-plane bt-fw"></i></a></li> -->
                     <!-- <li><a href="https://open.spotify.com/user/1238806903" class="btn-social btn-outline" target="_blank"><i class="text-light-green fab fab-spotify bt-fw"></i></a></li> -->
