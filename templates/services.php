37b7c8f12629c9f5a9f751f27700bb3a95c494e8
Cameron Van Orman <cameron@efellemedia.com>
2016-03-04 01:11:35 -0800

    ^ services

--- templates/services.php
+++ templates/services.php
@@ -37,25 +37,41 @@
             <div class="row">
                 <div class="col-lg-10 col-lg-offset-1">
                     <h2 class="text-center">Research Summary (unpublished):</h2>
-                    <hr class="small bg-coal">
+                    <hr class="small border-coal">
                     <div class="row">
                         <div class="col-sm-12">
                             <div class="service-item">
-                                <h4><strong>Van Orman, D.S.J. (In preparation). ‘The effects of acute aerobic exercise on auditory/spoken language</strong></h4>
+                                <h4>Van Orman, D.S.J. (In preparation). ‘The effects of acute aerobic exercise on auditory/spoken language</h4>
                                 <p>Learning of L2 English speaking Chinese university students learning L3 Russian’. This is a pilot experiment which provides evidence that word/phrasal and phonological acquisition of a foreign language is aided by exercise immediately prior to study for these participants. This experiment represents an original contribution to the psychology of language learning.</p>
                             </div>
                         </div>
                     </div>
-                    <hr class="small">
+                    <!-- /.row (nested) -->
+                    <hr class="small border-coal">
                     <div class="row">
                         <div class="col-sm-12">
                             <div class="service-item">
-                                <h4><strong>Van Orman, D.S.J. (In preparation). ‘Assessing ‘learning’ across three communication mediums: A mixed methods approach’.</strong></h4>
+                                <h4>Van Orman, D.S.J. (In preparation). ‘Assessing ‘learning’ across three communication mediums: A mixed methods approach’.</h4>
                                 <p>This is based a three-part task set for three groups (N=30) that shows clear advantages for text based communications overall and face-to-face communications to a lesser extent over video-conferencing in each of the three experimental tasks concerning memory. The tasks were original and adapted to Chinese-English speaking university students; they were (1) a long-term memory task, (2) a semantic and implicit memory of grammatical structures task, as well as (3) a nonword repetition task examining working memory, and long-term memory.</p>
                             </div>
                         </div>
                     </div>
                     <!-- /.row (nested) -->
+                    <hr class="small border-coal">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <h4>Van Orman, D.S.J. (In preparation). ‘A sociolinguistic review of language attitude research in four Chinese cities’.</h4>
+                            <p>This is a meta-analysis of matched-guise, questionnaire and qualitative studies assessing the attitudes Chinese university students from four cities (Hong Kong, Shanghai, Guangzhou and Urumqi) have about the local languages of these cities, of Mandarin, and of English. The findings are that local varieties are preferred in measures of solidarity (with the exception of Shanghai women), while Mandarin is seen as language of practical use. Finally, English is considered practical for Hong Kong, but maintains some traits for both solidarity and pragmatism in Urumqi.</p>
+                        </div>
+                    </div>
+                    <!-- /.row (nested) -->
+                    <hr class="small border-coal">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <h4>Wang, H. and Van Orman, D.S.J. (In preparation). ‘A case study assessing a Chinese high school’s students’ perceptions of linguistic, acculturative, psychological and sociocultural issues when studying abroad in the United States’.</h4>
+                            <p>Students (N=64), in this study, completed questionnaires, group discussions, and interviews to assess their preparedness for study abroad and their likelihood of future success. The results were codified and the participants were placed into integration, separation, assimilation and marginalization acculturative strategy groups. These categories have been reliable to predict study abroad students’ linguistic, social, psychological and in some circumstances, academic success by Ward and Kennedy (1992, 1993, 1994) and other authors. Follow up studies are planned from 2016-2018 as these students go to the United States.</p>
+                        </div>
+                    </div>
                 </div>
                 <!-- /.col-lg-10 -->
             </div>
