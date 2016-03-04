bc28a5830e7cefed269a3c510b4c06cc217bbc97
Cameron Van Orman <cameron@efellemedia.com>
2016-03-04 01:02:43 -0800

    ^ services

--- templates/services.php
+++ templates/services.php
@@ -5,62 +5,60 @@
 ?>
     <!-- Services -->
     <section id="services" class="services">
-        <div class="bg-egg-white">
-            <div class="container">
-                <div class="row text-center">
-                    <div class="col-lg-10 col-lg-offset-1">
-                        <h2>Research Interests</h2>
-                        <hr class="small border-coal">
-                        <div class="row">
-                            <div class="col-sm-6">
-                                <div class="service-item">
-                                    <h4><strong>Specific</strong></h4>
-                                    <p>Learning optimization; language aptitude; working and long-term memory; model of memory; factors contributing to memory retention and decay; phonological, segmental and phrasal acquisition, etc.</p>
-                                </div>
-                            </div>
-                            <div class="col-sm-6">
-                                <div class="service-item">
-                                    <h4><strong>Broad</strong></h4>
-                                    <p>Pyscholinguistics, first and second language acquisition, cross-cultural psychology, cognitive science, experimental psychology, sociology, sociobiology, sociolinguistics, social psychology, etc.</p>
-                                    
-                                </div>
+        <div class="container">
+            <div class="row text-center">
+                <div class="col-lg-10 col-lg-offset-1">
+                    <h2>Research Interests</h2>
+                    <hr class="small border-coal">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="service-item">
+                                <h4><strong>Specific</strong></h4>
+                                <p>Learning optimization; language aptitude; working and long-term memory; model of memory; factors contributing to memory retention and decay; phonological, segmental and phrasal acquisition, etc.</p>
                             </div>
                         </div>
-                        <!-- /.row (nested) -->
-                    </div>
-                    <!-- /.col-lg-10 -->
-                </div>
-                <!-- /.row -->
-            </div>
-        </div>
-        <div class="bg-coal">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-10 col-lg-offset-1">
-                        <h2>Research Summary (unpublished):</h2>
-                        <hr class="small">
-                        <div class="row">
-                            <div class="col-sm-6">
-                                <div class="service-item">
-                                    <h4><strong>Van Orman, D.S.J. (In preparation). ‘The effects of acute aerobic exercise on auditory/spoken language</strong></h4>
-                                    <p>Learning of L2 English speaking Chinese university students learning L3 Russian’. This is a pilot experiment which provides evidence that word/phrasal and phonological acquisition of a foreign language is aided by exercise immediately prior to study for these participants. This experiment represents an original contribution to the psychology of language learning.</p>
-                                </div>
-                            </div>
-                            <div class="col-sm-6">
-                                <div class="service-item">
-                                    <h4><strong>Van Orman, D.S.J. (In preparation). ‘Assessing ‘learning’ across three communication mediums: A mixed methods approach’.</strong></h4>
-                                    <p>This is based a three-part task set for three groups (N=30) that shows clear advantages for text based communications overall and face-to-face communications to a lesser extent over video-conferencing in each of the three experimental tasks concerning memory. The tasks were original and adapted to Chinese-English speaking university students; they were (1) a long-term memory task, (2) a semantic and implicit memory of grammatical structures task, as well as (3) a nonword repetition task examining working memory, and long-term memory.</p>
-                                </div>
+                        <div class="col-sm-6">
+                            <div class="service-item">
+                                <h4><strong>Broad</strong></h4>
+                                <p>Pyscholinguistics, first and second language acquisition, cross-cultural psychology, cognitive science, experimental psychology, sociology, sociobiology, sociolinguistics, social psychology, etc.</p>
+                                
                             </div>
                         </div>
-                        <!-- /.row (nested) -->
                     </div>
-                    <!-- /.col-lg-10 -->
+                    <!-- /.row (nested) -->
                 </div>
-                <!-- /.row -->
+                <!-- /.col-lg-10 -->
             </div>
-            <!-- /.container -->
+            <!-- /.row -->
         </div>
+    </section>
+    <section class="research bg-coal">
+        <div class="container">
+            <div class="row text-center">
+                <div class="col-lg-10 col-lg-offset-1">
+                    <h2>Research Summary (unpublished):</h2>
+                    <hr class="small">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="service-item">
+                                <h4><strong>Van Orman, D.S.J. (In preparation). ‘The effects of acute aerobic exercise on auditory/spoken language</strong></h4>
+                                <p>Learning of L2 English speaking Chinese university students learning L3 Russian’. This is a pilot experiment which provides evidence that word/phrasal and phonological acquisition of a foreign language is aided by exercise immediately prior to study for these participants. This experiment represents an original contribution to the psychology of language learning.</p>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="service-item">
+                                <h4><strong>Van Orman, D.S.J. (In preparation). ‘Assessing ‘learning’ across three communication mediums: A mixed methods approach’.</strong></h4>
+                                <p>This is based a three-part task set for three groups (N=30) that shows clear advantages for text based communications overall and face-to-face communications to a lesser extent over video-conferencing in each of the three experimental tasks concerning memory. The tasks were original and adapted to Chinese-English speaking university students; they were (1) a long-term memory task, (2) a semantic and implicit memory of grammatical structures task, as well as (3) a nonword repetition task examining working memory, and long-term memory.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.row (nested) -->
+                </div>
+                <!-- /.col-lg-10 -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container -->
     </section>
 
     <section class="parallax-section bg-filter-creek">        
