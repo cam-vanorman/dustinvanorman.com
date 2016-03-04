6d2f5832faed588c7d517e9a1f70e11a1266a53f
Cameron Van Orman <cameron@efellemedia.com>
2016-03-04 00:40:52 -0800

    ^ content single

--- templates/content-single.php
+++ templates/content-single.php
@@ -5,19 +5,21 @@
 				<div class="row">
 					<h1 class="text-center entry-title"><?php the_title(); ?></h1><hr class="small">
 					<?php 
-						the_date('D, M, Y', '<h4 class="text-center">', '</h4>');
+						the_date('M, d, Y', '<h4 class="text-center">', '</h4>');
 					?>
 				</div>
 			</div>
-			<div class="container">
-				<div class="row">            
-					<div class="col-lg-12">
-						<?php
-							// check if the post has a Post Thumbnail assigned to it.
-							if (has_post_thumbnail()) {
-								the_post_thumbnail();
-							}
-						?>
+			<div class="mar-t-20">
+				<div class="container">
+					<div class="row">            
+						<div class="col-lg-12">
+							<?php
+								// check if the post has a Post Thumbnail assigned to it.
+								if (has_post_thumbnail()) {
+									the_post_thumbnail();
+								}
+							?>
+						</div>
 					</div>
 				</div>
 			</div>
