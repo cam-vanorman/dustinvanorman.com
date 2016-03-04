c730762872f7078a7f32811bcba8357b7f8916b9
Cameron Van Orman <cameron@efellemedia.com>
2016-03-04 01:03:54 -0800

    ^ services

--- templates/services.php
+++ templates/services.php
@@ -39,13 +39,16 @@
                     <h2>Research Summary (unpublished):</h2>
                     <hr class="small">
                     <div class="row">
-                        <div class="col-sm-6">
+                        <div class="col-sm-12">
                             <div class="service-item">
                                 <h4><strong>Van Orman, D.S.J. (In preparation). ‘The effects of acute aerobic exercise on auditory/spoken language</strong></h4>
                                 <p>Learning of L2 English speaking Chinese university students learning L3 Russian’. This is a pilot experiment which provides evidence that word/phrasal and phonological acquisition of a foreign language is aided by exercise immediately prior to study for these participants. This experiment represents an original contribution to the psychology of language learning.</p>
                             </div>
                         </div>
-                        <div class="col-sm-6">
+                    </div>
+                    <hr class="small">
+                    <div class="row">
+                        <div class="col-sm-12">
                             <div class="service-item">
                                 <h4><strong>Van Orman, D.S.J. (In preparation). ‘Assessing ‘learning’ across three communication mediums: A mixed methods approach’.</strong></h4>
                                 <p>This is based a three-part task set for three groups (N=30) that shows clear advantages for text based communications overall and face-to-face communications to a lesser extent over video-conferencing in each of the three experimental tasks concerning memory. The tasks were original and adapted to Chinese-English speaking university students; they were (1) a long-term memory task, (2) a semantic and implicit memory of grammatical structures task, as well as (3) a nonword repetition task examining working memory, and long-term memory.</p>
