f52621088416149693ad522a35d3b782dfb1467c
Cameron Van Orman <cameron@efellemedia.com>
2016-03-07 00:40:47 -0800

    ^ rearranging templates at 1 in the morning

--- templates/call-to-action.php
+++ templates/call-to-action.php
@@ -15,11 +15,3 @@
 			</div>
 		</div>
 	</section>
-	<section class="parallax-section bg-chinese-lantern">
-		<div class="text-vertical-center">
-			<i class="btl bt-quote-right bt-3x"></i>
-			<hr class="small">
-			<h3>"I am not a genius. I am just a tremendous bundle of experience."</h3>
-			<h4>— R. Buckminster Fuller</h4>
-		</div>
-	</section>
--- templates/services.php
+++ templates/services.php
@@ -32,6 +32,14 @@
             <!-- /.row -->
         </div>
     </section>
+    <section class="parallax-section bg-chinese-lantern">
+        <div class="text-vertical-center">
+            <i class="btl bt-quote-right bt-3x"></i>
+            <hr class="small">
+            <h3>"I am not a genius. I am just a tremendous bundle of experience."</h3>
+            <h4>— R. Buckminster Fuller</h4>
+        </div>
+    </section>
     <section class="research">
         <div class="container">
             <div class="row">
