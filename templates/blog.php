e47c91333336a7b4fbd8ebc1f757c6b92b624a2c
Cameron Van Orman <cameron@efellemedia.com>
2016-03-08 00:48:30 -0800

    ^ blog

--- templates/blog.php
+++ templates/blog.php
@@ -14,42 +14,43 @@ $the_query = new WP_Query( $args );
 if ( $the_query->have_posts() ) : ?>
 
 <!-- Blog posts -->
-<section id="blog" class="container-fluid portfolio bg-egg-white">
-	<div class="row">
-		<div class="col-lg-12 text-center">
-			<h2>Recent Articles</h2>
-			<hr class="small border-coal">
+<section id="blog" class="blog bg-egg-white">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12 text-center">
+				<h2>Recent Articles</h2>
+				<hr class="small border-coal">
+			</div>
 		</div>
-	</div>
-	<div class="row items">
-	
-	<?php
-	// Start the Loop
-	while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-
+		<div class="row items">
 		
-		<div class="col-md-4">
-			<h3><?php the_title(); ?></h3>
-			<hr class="small">
-			<p class="lead"><?php the_excerpt(); ?></p>
-		</div>
-		
-	<?php 
-	endwhile; // End the Loop 
-	?>
+		<?php
+		// Start the Loop
+		while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 
-	</div> <!-- ./ row items -->
-	
+			
+			<div class="col-md-4">
+				<h3><?php the_title(); ?></h3>
+				<hr class="small">
+				<p class="lead"><?php the_excerpt(); ?></p>
+			</div>
+			
+		<?php 
+		endwhile; // End the Loop 
+		?>
+
+		</div> <!-- ./ row items -->
+		
 	<?php else:
 		// If no posts match this query, output this text.
-		_e( '<div class="text-vertical-center">
+		_e( '<div class="row"><div class="text-vertical-center">
             <i class="btl bt-quote-right bt-3x"></i>
             <hr class="small">
             <h3>"The good writer seems to be writing about himself, but has his eye always on that thread of the Universe which runs through himself and all things."</h3>
             <h4>— Ralph Waldo Emerson</h4>
-   			</div>', 'dustinvanorman.com' );
+   			</div></div>', 'dustinvanorman.com' );
 	endif;
-	wp_reset_postdata();
- 
+	wp_reset_postdata();	 
 ?>
+	</div>
 </section>
