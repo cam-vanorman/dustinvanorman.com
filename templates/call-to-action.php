0da16ec6a4d516cde50cff9d506cbaa76c0ac9d5
Cameron Van Orman <cameron@efellemedia.com>
2016-12-03 15:43:29 -0800

    ^ resume

--- templates/call-to-action.php
+++ templates/call-to-action.php
@@ -10,7 +10,7 @@
 			<div class="row">
 				<div class="col-lg-12 text-center">
 					<h2>Interested in working together?</h2>
-					<a href="/wp-content/uploads/2016/09/Dustin-Van-Orman-Summary-and-Full-CV.pdf" class="btn-social btn-outline" rel="nofollow" data-toggle="tooltip" data-placement="bottom" title="View my Resume"><i class="btl bt-link bt-fw"></i></a>
+					<a href="/wp-content/uploads/2016/12/Dustin-Van-Orman-CV-Updated.pdf" class="btn-social btn-outline" rel="nofollow" data-toggle="tooltip" data-placement="bottom" title="View my Resume"><i class="btl bt-link bt-fw"></i></a>
 				</div>
 			</div>
 		</div>
